e0de6427cb946c71e91e45d0e85ae43717f79d38
Rick Geersing <rickgeersing@mac.com>
2024-11-25 22:11:07 +0100

    refactor: change to vitePreprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
 import adapter from '@sveltejs/adapter-node';
-// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { sveltePreprocess } from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: sveltePreprocess(),
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter()
 	}
