f1b1be306d62682238cf9a307f3a6c179b3a05cf
Rick Geersing <rickgeersing@mac.com>
2024-11-25 22:55:22 +0100

    refactor: add background to page, so content is more visible

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,6 +15,7 @@
 	<div class="background">
 		<Game />
 	</div>
+	<div class="background-glass"></div>
 	<div class="content">
 		<h1>Rick Geersing</h1>
 		<h2>Software Developer</h2>
@@ -23,6 +24,12 @@
 </div>
 
 <style lang="scss">
+	@mixin breakpoint($point, $type: max, $direction: width) {
+		@media only screen and (#{$type}-#{$direction}: $point) {
+			@content;
+		}
+	}
+
 	.hero {
 		height: 100vh;
 		width: 100%;
@@ -35,21 +42,52 @@
 			flex-direction: column;
 			justify-content: center;
 			align-items: center;
+			gap: 5px;
 
 			h1 {
 				margin: 0;
-				font-size: 120px;
 				font-weight: 400;
 				line-height: 75px;
 				user-select: none;
+				font-size: 120px;
+				text-align: center;
+
+				@include breakpoint(768px) {
+					font-size: 80px;
+					line-height: 50px;
+				}
+
+				@include breakpoint(480px) {
+					font-size: 60px;
+					line-height: 40px;
+				}
 			}
 
 			h2 {
 				margin: 0;
-				font-size: 45px;
 				font-family: 'Montserrat', sans-serif;
 				user-select: none;
+				font-size: 45px;
+
+				@include breakpoint(768px) {
+					font-size: 30px;
+				}
+
+				@include breakpoint(480px) {
+					font-size: 20px;
+				}
 			}
+		}
+
+		.background-glass {
+			position: absolute;
+			height: 100%;
+			width: 100%;
+			background: rgba(255, 255, 255, 0.2);
+			border-radius: 16px;
+			box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+			backdrop-filter: blur(5px);
+			-webkit-backdrop-filter: blur(5px);
 		}
 	}
 </style>
