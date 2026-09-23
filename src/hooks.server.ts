302098a4eca439a6aa0246d4d6d9f631356f837d
Rick Geersing <rickgeersing@mac.com>
2026-09-23 20:52:41 +0200

    fix: remove unused shit, remove npm and installed pnpm

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,7 @@
-import { retrieveSession } from "$lib/server/session/managment";
-import type { Handle } from "@sveltejs/kit";
+import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-    event.locals.session = await retrieveSession(event.cookies);
-    const response = await resolve(event);
+	const response = await resolve(event);
 
-    return response;
+	return response;
 };
--- src/routes/console/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-
-export const load = (async ({ locals }) => {
-
-    return {
-        session: locals.session,
-    };
-}) satisfies LayoutServerLoad;
