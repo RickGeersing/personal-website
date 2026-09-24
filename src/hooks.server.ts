979ed2dbc94ccd0bc5e2f6a34f858099aa7d2b29
Rick Geersing <rickgeersing@mac.com>
2026-09-24 21:44:57 +0200

    feat: add cache headers to website

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,6 +2,14 @@ import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event);
+	if (
+		(event.request.method === 'GET' || event.request.method === 'HEAD') &&
+		event.url.pathname === '/' &&
+		response.status === 200 &&
+		!response.headers.has('cache-control')
+	) {
+		response.headers.set('cache-control', 'public, max-age=0, s-maxage=3600');
+	}
 
 	return response;
 };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,8 @@
 	import ContactForm from '$client/components/contact-form/contactForm.svelte';
 	import { writable } from 'svelte/store';
 	import { setContext } from 'svelte';
+	import newAmsterdamFont from '$client/assets/NewAmsterdam-Regular.woff2?url';
+	import siteIcon from '$client/assets/favicon.ico?url';
 	interface Props {
 		children?: import('svelte').Snippet;
 	}
@@ -16,8 +18,8 @@
 </script>
 
 <svelte:head>
-	<link rel="preload" href="/fonts/NewAmsterdam-Regular.woff2" as="font" crossorigin="anonymous" />
-	<link rel="preload" href="/fonts/NewAmsterdam-Regular.woff" as="font" crossorigin="anonymous" />
+	<link rel="icon" href={siteIcon} />
+	<link rel="preload" href={newAmsterdamFont} as="font" type="font/woff2" crossorigin="anonymous" />
 	<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
 </svelte:head>
 
